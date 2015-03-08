395f39ae367884f5084f022c926e7950246197b3
Julien Roland <juroland@gmail.com>
2015-03-08 20:54:51 +0100

    Up

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; Inspired from :
+;;   - https://github.com/purcell/emacs.d
+;;   - https://github.com/bbatsov/prelude
+
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'init-packages)
--- lisp/init-window.el
+++ lisp/init-window.el
@@ -4,4 +4,12 @@
   (menu-bar-mode -1)
   (scroll-bar-mode -1))
 
+;; Undo (redo) window configuration with C-c <left> (C-c <right>)
+(winner-mode 1)
+
+;; Visual window switching with C-x o
+(require 'switch-window)
+(global-set-key (kbd "C-x o") 'switch-window)
+(setq switch-window-shortcut-style 'alphabet)
+
 (provide 'init-window)
